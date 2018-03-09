435a1d8e7d364c498abaede5355b3861483d07ab
Nathan Schmidt <onetoothreefor@hotmail.com>
2018-03-09 11:35:59 -0500

    oAuth for google set up

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
+const passportConfig = require("./services/passport");
 
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send({ yo: "sup" });
-});
+console.log("it's working!");
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || app.listen(5000);
 
