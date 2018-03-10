d349dc80099e7d489e2eb50f0def051779c123d5
Nathan Schmidt <onetoothreefor@hotmail.com>
2018-03-09 19:17:11 -0500

    no user

--- models/user.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
-
-const userSchema = new Schema({
-  googleId: "string"
-});
-//Ihave changed the name user to User again
-mongoose.model("users", userSchema);
