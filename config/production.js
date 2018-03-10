2422eb8261a45407c1759a7e3b12cec88b7dd47c
Nathan Schmidt <onetoothreefor@hotmail.com>
2018-03-10 13:16:49 -0500

    working heroku production server

--- config/production.js
+++ config/production.js
@@ -1,7 +1,6 @@
 module.exports = {
-    googleClientID:
-      "29489159029-ido16bjifdns7l3vfa355hj5vvolraos.apps.googleusercontent.com",
-    googleClientSecret: "GFcOLibSz-eJs9DGsM5UN_qz",
-    mongoURI: "mongodb://Nathan:nahtan780@ds163418.mlab.com:63418/natabase",
-    cookieKey: "jfjdfkdjfldkjfsnirrnrmntm"
-  };
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
