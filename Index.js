50a04106d24662eb8d6e7f1e625fb4450510913d
Nathan Schmidt <onetoothreefor@hotmail.com>
2018-03-12 15:34:18 -0400

    added billing and client side app, possibly check .env

--- Index.js
+++ Index.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
+const bodyParser = require("body-parser");
 const models = require("./models/User");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+
 const express = require("express");
 const passportConfig = require("./services/passport");
 const app = express();
 
 mongoose.connect(keys.mongoURI);
-
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -18,8 +20,19 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 console.log("it's working!");
-require("./routes/authRoutes")(app);
 
+require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
+
+if (process.env.NODE_ENV === "production") {
+  //express will serve production assets
+  app.use(express.static("client/build"));
+  //express will serve index.html if it does not recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 const PORT = process.env.PORT || app.listen(5000);
 
 app.listen(PORT);
--- models/User.js
+++ models/User.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  googleId: "string"
+  googleId: "string",
+  credits: {
+    type: Number,
+    default: 0
+  }
 });
 //Ihave changed the name user to User again
 mongoose.model("users", userSchema);
