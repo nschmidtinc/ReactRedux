7a36952a65e7857ab1a2f62b3607c48960682b56
Nathan Schmidt <onetoothreefor@hotmail.com>
2018-03-15 18:43:30 -0400

    retry heroku update

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -5,9 +5,6 @@ const Landing = () => {
     <div style={{ textAlign: "center" }}>
       <h1>NateMail</h1>
       Collect feedback from Cients
-      <Link to="/surveys/new" className="btn-floating btn-large red">
-        <i className="material-icons">add</i>
-      </Link>
     </div>
   );
 };
