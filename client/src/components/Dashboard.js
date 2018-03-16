00508ad0f45c5d086aa0de8b85c2b097401bc726
Nathan Schmidt <onetoothreefor@hotmail.com>
2018-03-16 13:30:00 -0400

    finished survey helper heroku deploy

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SurveyList from "./SurveyList";
 //import SurveyList from "./surveys/SurveyList";
 
 const Dashboard = () => {
   return (
     <div>
+      <div>
+        <SurveyList />
+      </div>
       <div className="fixed-action-btn">
         <Link to="/surveys/new" className="btn-floating btn-large red">
           <i className="material-icons">add</i>
